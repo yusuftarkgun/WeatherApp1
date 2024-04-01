443f899cdcaf6b8e3f061ba9c76ce4305330450e
yusuftarikgun <yusuftarkgun@hotmail.com>
2024-04-01 12:22:24 +0300

    ServiceModele'e icon için eklemeler yapıldı.
    TableView ve CurrentTemp için iconlara yer açıldı. İconCode eklendi.

--- WeatherApp/Weather/Model/ServiceModel.swift
+++ WeatherApp/Weather/Model/ServiceModel.swift
@@ -1,67 +1,5 @@
-//
-//  ServiceModel.swift
-//  WeatherApp
-//
-//  Created by Yusuf Tarık Gün on 28.03.2024.
 import Foundation
-struct Welcome: Codable {
-    let lat, lon: Double
-    let timezone: String
-    let timezoneOffset: Int
-    let minutely: [Minutely]
-    let hourly: [Hourly]
-    let daily: [Daily]
 
-    enum CodingKeys: String, CodingKey {
-        case lat, lon, timezone
-        case timezoneOffset = "timezone_offset"
-        case minutely, hourly, daily
-    }
-}
-
-// MARK: - Daily
-struct Daily: Codable {
-    let dt, sunrise, sunset, moonrise: Int
-    let moonset: Int
-    let moonPhase: Double
-    let temp: Temp
-    let feelsLike: FeelsLike
-    let pressure, humidity: Int
-    let dewPoint, windSpeed: Double
-    let windDeg: Int
-    let windGust: Double
-    let weather: [Weather]
-    let clouds: Int
-    let pop: Double
-    let rain: Double?
-    let uvi: Double
-
-    enum CodingKeys: String, CodingKey {
-        case dt, sunrise, sunset, moonrise, moonset
-        case moonPhase = "moon_phase"
-        case temp
-        case feelsLike = "feels_like"
-        case pressure, humidity
-        case dewPoint = "dew_point"
-        case windSpeed = "wind_speed"
-        case windDeg = "wind_deg"
-        case windGust = "wind_gust"
-        case weather, clouds, pop, rain, uvi
-    }
-}
-
-// MARK: - FeelsLike
-struct FeelsLike: Codable {
-    let day, night, eve, morn: Double
-}
-
-// MARK: - Temp
-struct Temp: Codable {
-    let day, min, max, night: Double
-    let eve, morn: Double
-}
-
-// MARK: - Weather
 struct Weather: Codable {
     let id: Int
     let main: Main
@@ -83,36 +21,9 @@ enum Main: String, Codable {
     case clouds = "Clouds"
     case rain = "Rain"
 }
-
-// MARK: - Hourly
-struct Hourly: Codable {
-    let dt: Int
-    let temp, feelsLike: Double
-    let pressure, humidity: Int
-    let dewPoint, uvi: Double
-    let clouds, visibility: Int
-    let windSpeed: Double
-    let windDeg: Int
-    let windGust: Double
-    let weather: [Weather]
-    let pop: Int
-
-    enum CodingKeys: String, CodingKey {
-        case dt, temp
-        case feelsLike = "feels_like"
-        case pressure, humidity
-        case dewPoint = "dew_point"
-        case uvi, clouds, visibility
-        case windSpeed = "wind_speed"
-        case windDeg = "wind_deg"
-        case windGust = "wind_gust"
-        case weather, pop
-    }
-}
-
-// MARK: - Minutely
-struct Minutely: Codable {
-    let dt, precipitation: Int
+struct Temp: Codable {
+    let day, min, max, night: Double
+    let eve, morn: Double
 }
 
 struct WeatherData: Codable {
@@ -129,9 +40,18 @@ struct CurrentWeather: Codable {
 struct DailyWeather: Codable {
     let dt: Date
     let temp: Temp
+    let weather: [Weather]
+    var iconCode: String? {
+      
+        return weather.first?.icon
+    }
+}
+
+        
+  
     //let summary: String
     //let icon: String
-}
+
 
 struct Temperature: Codable {
     let day: Double
